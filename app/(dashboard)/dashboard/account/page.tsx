5d98294ff9d7421bf140030fd15509c4cb562c0e
Meguenni Achour <achourphp@gmail.com>
2025-03-23 07:35:12 +0100

    Implement account settings page with user information form and loading state

--- app/(dashboard)/dashboard/account/page.tsx
+++ app/(dashboard)/dashboard/account/page.tsx
@@ -1,11 +1,90 @@
-import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
+"use client";
 
-export default async function Page() {
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { IconLoader } from "@tabler/icons-react";
+
+import { authClient } from "@/lib/auth-client";
+
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function getUser() {
+    const { data: session } = await authClient.getSession();
+    // if (!session?.user) {
+    //   return router.push("/login");
+    // }
+    console.log(session);
+    return session;
+  }
+
+  useEffect(() => {
+    getUser().then((data) => {
+      setFullname(data?.user?.name ?? ""); // Use empty string as fallback
+      setEmail(data?.user?.email ?? "");
+    });
+  }, []);
+
   return (
     <>
       <div className="px-4 lg:px-6">
-        <h1>Account Setting...</h1>
+        <h1 className="text-lg font-medium">Account Setting</h1>
+        <p className="text-sm text-muted-foreground mb-2">
+          Edit your account information
+        </p>
+
+        <form className="lg:w-1/2">
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Full Name</Label>
+              <Input
+                onChange={(e) => setFullname(e.target.value)}
+                value={fullname}
+                id="name"
+                type="text"
+                placeholder="Achour Meguenni"
+                required
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                id="email"
+                type="email"
+                placeholder="me@example.com"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Button disabled={loading} type="submit" className="w-full">
+                {loading ? (
+                  <IconLoader className="animate-spin" stroke={2} />
+                ) : (
+                  "Save"
+                )}
+              </Button>
+            </div>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Forgot your password?{" "}
+            <a href="/login" className="underline underline-offset-4">
+              Reset password
+            </a>
+          </div>
+        </form>
       </div>
     </>
   );
