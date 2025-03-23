7ee7c1efceb9e37d70758798c95808e5f9e7b9b8
Meguenni Achour <achourphp@gmail.com>
2025-03-23 05:51:30 +0100

    Refactor Prisma client initialization and add VSCode settings

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export { prisma };
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
