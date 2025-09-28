6faad8607334dd3fcf7478b4c1e2ee3879514186
achour <achourphp@gmail.com>
2025-09-28 22:36:06 +0100

    refactor: remove runtime configuration from middleware

--- middleware.ts
+++ middleware.ts
@@ -20,6 +20,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: 'nodejs',
   matcher: ["/dashboard", "/login", "/signup"], // Apply middleware to these routes
 };
